ef9000182a3c4bf57f6b5dc6fe25ddc62a8286bf
SubasH5552 <musharafz2k3@gmail.com>
2023-10-08 15:15:48 +0530

    sue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,9 @@ const Contact = () => {
             <div className='pb-8'>
                 <p className=' text-4xl inline border-b-4 border-slate-700 text-white '>Contact</p>
             </div>
-            <input className='bg-slate-200 p-2 rounded' type='text' placeholder='Name' name='name'/>
-            <input className='my-4 p-2 bg-slate-200 rounded' type='email' placeholder='Email' name='email'/>
-            <textarea className='bg-slate-200 p-2 rounded' name='message' rows={10} placeholder='Message'></textarea>
+            <input className='bg-slate-200 p-2 rounded' type='text' required placeholder='Name' name='name'/>
+            <input className='my-4 p-2 bg-slate-200 rounded' type='email' required placeholder='Email' name='email'/>
+            <textarea className='bg-slate-200 p-2 rounded' name='message' rows={10} required placeholder='Message'/>
             <button className='text-white border-2 hover:bg-stone-400 hover:border-stone-700 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
